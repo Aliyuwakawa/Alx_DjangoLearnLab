c81c8c6e6d71ac3ab5c153b51136f4b2a0ec348c
Aliyu Saddam Wakawa <aliyuwakawa76@gmail.com>
2024-08-15 18:25:57 +0100

    as

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -54,3 +54,20 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     instance.userprofile.save()
 
 
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
+
+
