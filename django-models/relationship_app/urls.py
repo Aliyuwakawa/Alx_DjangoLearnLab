7f73ac2702e6d9d2bb633515150cfb7ce18b2aa1
Aliyu Saddam Wakawa <aliyuwakawa76@gmail.com>
2024-08-15 15:43:59 +0100

    c

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,3 +6,12 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
 
+from django.urls import path
+from .views import login_view, logout_view, register_view
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+]
+
