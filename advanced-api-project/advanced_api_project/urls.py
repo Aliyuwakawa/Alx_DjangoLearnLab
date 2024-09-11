2f300accbbbf51dcbb312074adee960babf40c7b
Aliyu Saddam Wakawa <aliyuwakawa76@gmail.com>
2024-09-11 18:06:01 +0100

    a

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # Import 'include' to include app-specific URLs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Include the 'api' app's URL configurations
 ]
-from django.urls import path
-from . import views
+
+
+
 
