56367315eb9de3775b72789f174d3b14e9fe6f28
Aliyu Saddam Wakawa <aliyuwakawa76@gmail.com>
2024-09-17 17:39:32 +0100

    a

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -8,3 +8,14 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']  # Assuming Comment model has a 'content' field
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 4}),
+        }
