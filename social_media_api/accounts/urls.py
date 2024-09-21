1d9ceb45adf62626c1a4a07891d61c8ddc7d125c
Aliyu Saddam Wakawa <aliyuwakawa76@gmail.com>
2024-09-21 16:57:09 +0100

    b

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import RegisterView, ProfileView  # Ensure these views are imported
 
 urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='register'),
+    path('register/', RegisterView.as_view(), name='register'),  # User registration
+    path('login/', obtain_auth_token, name='login'),  # Token-based login
+    path('profile/', ProfileView.as_view(), name='profile'),  # User profile management
 ]
