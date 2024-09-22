7986925b550dc1142b402cc5c053a277f13da34a
Aliyu Saddam Wakawa <aliyuwakawa76@gmail.com>
2024-09-22 11:16:26 +0100

    c

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,7 +6,33 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+    
 
     def __str__(self):
         return self.username
+    
+class CustomUser(AbstractUser):
+    following = models.ManyToManyField(
+        'self', 
+        through='UserFollow', 
+        related_name='followers',
+        symmetrical=False
+    )
+
+    def __str__(self):
+        return self.username
+
+class UserFollow(models.Model):
+    user = models.ForeignKey(CustomUser, related_name='following_set', on_delete=models.CASCADE)
+    followed_user = models.ForeignKey(CustomUser, related_name='followers_set', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'followed_user')
+
+    def __str__(self):
+        return f'{self.user} follows {self.followed_user}'
+
+    
+    
 
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -17,3 +17,29 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+    
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
+
+class FollowUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({'status': 'followed'}, status=status.HTTP_200_OK)
+
+class UnfollowUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
+
+    
+
