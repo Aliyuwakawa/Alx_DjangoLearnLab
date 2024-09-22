ca91a79bf30f8286f3557d485677788d9b915259
Aliyu Saddam Wakawa <aliyuwakawa76@gmail.com>
2024-09-22 11:32:50 +0100

    a

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -22,3 +22,18 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+    from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
+
+class UserFeedView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        # Get posts from users the current user follows
+        user = self.request.user
+        return Post.objects.filter(author__in=user.following.all()).order_by('-created_at')
+
+
